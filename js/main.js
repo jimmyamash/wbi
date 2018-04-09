aee9b6a94459814997f4b9af10abac3ffaebb084
Jimmy Amash <jimmy@hellojumper.com>
2018-04-09 12:16:54 -0700

    Optimizing JavaScript/animations

--- js/main.js
+++ js/main.js
@@ -11,38 +11,38 @@ $(document).ready(function(){
             $(".faq-question").addClass("active");
         }
     });
-    $(".row:not(.row-header):not(.row-main-atf)").appear(function(){
-        $(this).addClass("animate");
-    },{accX: 50, accY: -300});
+    // $(".row:not(.row-header):not(.row-main-atf)").appear(function(){
+    //     $(this).addClass("animate");
+    // },{accX: 50, accY: -300});
     $("input:not([type='submit'])").on("blur", function(){
         var trim = $(this).val().trim();
         $(this).val(trim);
     });
 
-    var didScroll = false;
-
-    window.onscroll = doThisStuffOnScroll;
-
-    function doThisStuffOnScroll() {
-        didScroll = true;
-    }
-
-    setInterval(function() {
-        if(didScroll) {
-            didScroll = false;
-            $(".row:not(.row-header):not(.row-main-atf)").appear(function(){
-                $(this).addClass("animate");
-            },{accX: 50, accY: -300});
-        }
-    }, 100);
+    // var didScroll = false;
+    //
+    // window.onscroll = doThisStuffOnScroll;
+    //
+    // function doThisStuffOnScroll() {
+    //     didScroll = true;
+    // }
+    //
+    // setInterval(function() {
+    //     if(didScroll) {
+    //         didScroll = false;
+    //         $(".row:not(.row-header):not(.row-main-atf)").appear(function(){
+    //             $(this).addClass("animate");
+    //         },{accX: 50, accY: -300});
+    //     }
+    // }, 100);
 
     // $(window).scroll(function(){
     //     $(".row:not(.row-header):not(.row-main-atf)").appear(function(){
     //         $(this).addClass("animate");
     //     },{accX: 50, accY: -300});
     // });
-    $(".row-header").addClass("animate");
-    $(".row-main-first").addClass("animate");
+    // $(".row-header").addClass("animate");
+    // $(".row-main-first").addClass("animate");
 
     $(".faq-button-toggle").click(function(){
         $('html,body').animate({ scrollTop: 0 }, 300);
